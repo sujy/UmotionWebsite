c57086886168c269e7c57170b5ded529bf16858b
Su JiaYang / Young So <sujy_passion@foxmail.com>
2014-12-13 14:50:59 +0800

    Update models.py处理冲突
    修改Project models.py

--- umotion/Project/models.py
+++ umotion/Project/models.py
@@ -2,19 +2,6 @@ from django.db import models
 from User.models import User
 # Create your models here.
 class RoadMap(models.Model):
-<<<<<<< HEAD
-    time = models.DateField(blank=True)
-    content = models.TextField(max_length=300)
-# branchd
-class Project(models.Model):
-    title = models.TextField(max_length=51200)
-    brief = models.TextField(max_length=51200)
-    member = models.ManyToManyField(User, related_name='project_member')
-    leader = models.ForeignKey(User)
-    follower = models.ManyToManyField(User, related_name='project_follower')
-    roadmap = models.ForeignKey(RoadMap)
-    category = models.CharField(max_length=100)
-=======
 	time = models.DateField(blank=True)
 	content = models.TextField(max_length=300)
 # branch
@@ -26,5 +13,4 @@ class Project(models.Model):
 	follower = models.ManyToManyField(User, related_name='project_follower')
 	roadmap = models.ForeignKey(RoadMap)
 	category = models.CharField(max_length=100)
->>>>>>> upstream/master
 
